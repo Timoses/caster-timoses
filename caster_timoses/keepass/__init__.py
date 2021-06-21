f9f5f9764f82c3a3d1cc2d103b2b82095260b0ad
Timo Funke <timo.funke@biotronik.com>
2021-06-21 20:10:34 +0200

    Keepass introduce small delay on shock

--- caster_timoses/keepass/__init__.py
+++ caster_timoses/keepass/__init__.py
@@ -48,7 +48,7 @@ class KeepassPlugin(Plugin):
             mapping.update({'key pass ' + words: Text(entry.password)})
             mapping.update({'key user ' + words: Text(entry.username)})
             mapping.update({'key tab ' + words: Text(entry.username) + Key('tab') + Text(entry.password)})
-            mapping.update({'key shock ' + words: Text(entry.username) + Key('enter') + Text(entry.password)})
+            mapping.update({'key shock ' + words: Text(entry.username) + Key('enter/20') + Text(entry.password) + Key('enter')})
 
         rule = MappingRule(mapping=mapping)
         grammar = Grammar(name="Keepass")
